d6ab8fa1a8ad9992f0091d84b566941a4cdea2aa
Thang <viktor.thang.phan@gmail.com>
2021-06-17 17:31:54 -0400

    Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import CallIcon from "@material-ui/icons/Call";
+import EmailIcon from "@material-ui/icons/Email";
+import "./App.css";
+import Portrait from './portrait.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="information">
+          <h2>Welcome to the V Society</h2>
+          <h1>Viktor Phan</h1>
+          <p>
+            10 years participating in self declared charitable activities, Lorem
+            ipsum dolor sit amet, consectetur adipisicing elit. Ullam error hic
+            ipsam praesentium accusantium minima laudantium! Reprehenderit harum
+            vel incidunt reiciendis assumenda maiores unde porro praesentium,
+            doloribus dignissimos culpa maxime.
+          </p>
+          <div className="options">
+            <p>
+              Call me <CallIcon />
+            </p>
+            <p>
+              Email me <EmailIcon />
+            </p>
+          </div>
+        </div>
+        <img src={Portrait} alt="" />
+      </div>
     </div>
   );
 }
